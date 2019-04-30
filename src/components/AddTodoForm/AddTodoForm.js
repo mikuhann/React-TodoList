2dc41d8b01a0f231b50a746d10688e47a29fc3b1
mikuhann <ng.kano@gmail.com>
2019-04-30 19:21:02 +0300

    add input functionality

--- src/components/AddTodoForm/AddTodoForm.js
+++ src/components/AddTodoForm/AddTodoForm.js
@@ -3,19 +3,39 @@ import React, {Component} from 'react';
 import './AddTodoForm.css';
 
 export default class AddTodoForm extends Component {
+  state = {
+    text: ''
+  };
+  onInputChange = (e) => {
+    this.setState({
+      text: e.target.value
+    });
+  };
+  onSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.text.length) {
+      this.props.addTodo(this.state.text);
+      this.setState({
+        text: ''
+      });
+    }
+  };
   render() {
-    const { addTodo } = this.props;
+    const { text } = this.state;
     return (
-      <div className='add-todo-form'>
+      <form className='add-todo-form'
+            onSubmit={ this.onSubmit }>
         <div className="form-group d-flex mt-3">
-          <input type="text" placeholder='Input your todo' className='form-control mr-2'/>
-          <button type='submit'
-                  className='btn btn-outline-secondary'
-                  onClick={ () => addTodo('Hello World')}>
+          <input type="text"
+                 placeholder='Input your todo'
+                 className='form-control mr-2'
+                 onChange={ this.onInputChange }
+                 value={ text } />
+          <button className='btn btn-outline-secondary'>
             Add
           </button>
         </div>
-      </div>
+      </form>
     );
   }
 }
